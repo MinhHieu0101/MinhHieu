43f93ccdf8cb4d189351a1d4cf84b72b0b544652
MinhHieu0101 <tung198q@gmail.com>
2022-08-10 17:12:00 +0700

    bai tren lop

---
+++ buoi 5/tim so nguyen to.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main (){
+	int n;
+    printf("nhap so nguyen =");
+    scanf("%d",&n);
+    
+    int y =1;
+	while(y<=n){
+		y++;
+	} 
+	printf("so nguyen to cua n =%d",n);
+}
